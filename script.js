bfe6b7cdb0385b094a3d9f0e48b71afe7a0fee62
Vigneshwaran8220 <145905413+Vigneshwaran8220@users.noreply.github.com>
2023-11-06 14:18:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+for(let i=0; i<6; i++){
+    if(i==3){
+    alert("hey the loop has runed three times");
+    }
+    console.log(i);
+}
+for(let i=0; i<6; i++){
+    if(i==3){
+    prompt("hey the loop has runed three times","hii");
+    }
+    console.log(window);
+    window.location.replace("https://www.google.com/search?gs_ssp=eJzj4tVP1zc0TMsuLijLza00YPQSyMnPTi3OUMhOzEtMTyxKzAIAu-YLjQ&q=lokesh+kanagaraj&rlz=1C1UEAD_enIN1077IN1077&oq=lok&gs_lcrp=EgZjaHJvbWUqDQgCEC4YgwEYsQMYgAQyBggAEEUYOTIPCAEQLhhDGIMBGLEDGIoFMg0IAhAuGIMBGLEDGIAEMg8IAxAAGEMYgwEYsQMYigUyDQgEEAAYgwEYsQMYgAQyCQgFEAAYQxiKBTIJCAYQABhDGIoFMgoIBxAAGLEDGIAEMg0ICBAAGIMBGLEDGIAEMgcICRAAGI8C0gEIMjgwMGowajeoAgCwAgA&sourceid=chrome&ie=UTF-8")
+}
+for (let i=0; i<6; i++){
+    console.log(i);
+}
+console.log(window);
+
+function newFunction(){
+    console.log("hey i'm the function");
+    alert("hi there")
+}
+setTimeout(newSecons,5000)
+
